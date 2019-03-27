055e9218bbeab9a2e5f25a3abc407f5e47cd323d
freddycro <freddy10100@hotmail.com>
2019-03-27 15:09:26 +0800

    fixed prereder

--- src/components/FBComment.vue
+++ src/components/FBComment.vue
@@ -1,29 +1,17 @@
 <template>
   <div class="fb-comment-block">
-    <div class="fb-comments" :data-href="setProps('href')" data-numposts="5" data-width="100%" data-order-by="reverse_time"></div>
+    <div class="fb-comments" :data-href="href" data-numposts="5" data-width="100%" data-order-by="reverse_time"></div>
   </div>
 </template>
 
 <script>
-import setProps from '../mixin/setProps.js'
-
 export default {
   name: 'FbComment',
-  mixins: [setProps],
   props: {
     href: {
       type: String
     },
-    jsonProps: {
-      type: Object,
-      default: null
-    }
   },
-  created () {
-    if (this.$props.href === undefined && this.$props.jsonProps === null) {
-      console.error('請輸入 href ex: <fb-comment> href="{Url}"></fb-comment>')
-    }
-  }
 }
 </script>
 
